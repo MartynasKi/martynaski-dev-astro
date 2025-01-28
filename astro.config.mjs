d72dabecafe23412b3811af51c71a81e413ac104
MartynasKi <github@martynaski.dev>
2025-01-28 12:09:04 +0100

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  })
+    // adapter: cloudflare({
+    //     platformProxy: {
+    //         enabled: true,
+    //     },
+    // }),
+
+    vite: {
+        resolve: {
+            alias: {
+                "@": "/src", // Use @ for the src directory
+            },
+        },
+    },
+
+    integrations: [tailwind()],
 });
